f93c1bd5347c3e4c781aa920c078493ebc1f773b
dwivedia988 <153066681+dwivedia988@users.noreply.github.com>
2024-03-13 03:45:48 +0530

    Multi language support page

--- src/components/Header.js
+++ src/components/Header.js
@@ -43,30 +43,39 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
+  const handleGptSearchClick = () => {
+    // Toggle GPT Search
+    dispatch(toggleGptSearchView());
+  };
+
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 justify-between flex">
       <img className="w-44" src={LOGO} alt="logo" />
 
       {user && (
         <div className="flex p-2 justify-between">
-          {/* {showGptSearch && (
-          <select
-            className="p-2 m-2 bg-gray-900 text-white"
-            onChange={handleLanguageChange}
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
+            onClick={handleGptSearchClick}
           >
-            {SUPPORTED_LANGUAGES.map((lang) => (
-              <option key={lang.identifier} value={lang.identifier}>
-                {lang.name}
-              </option>
-            ))}
-          </select>
-        )}
-        <button
-          className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
-          onClick={handleGptSearchClick}
-        >
-          {showGptSearch ? "Homepage" : "GPT Search"}
-        </button> */}
+            {showGptSearch ? "Homepage" : "GPT Search"}
+          </button>
           <img
             className="hidden md:block w-12 h-12"
             alt="usericon"
